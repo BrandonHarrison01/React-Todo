6aacfbd4df762cacef49721180ffb5e4cfecc961
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-29 16:55:28 -0600

    WIP some progress

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,29 @@
+const Todo = [
+    {
+        task: "Take out the trash",
+        id: 1,
+        completed: true
+    },
+    {
+        task: "Grocery shopping",
+        id: 2,
+        completed: false
+    },
+    {
+        task: "Clean gecko tank",
+        id: 3,
+        completed: false
+    },
+    {
+        task: "Mow lawn",
+        id: 4,
+        completed: true
+    },
+    {
+        task: "Catch up on Arrested Development",
+        id: 5,
+        completed: false
+    }
+]
+
+export default Todo
